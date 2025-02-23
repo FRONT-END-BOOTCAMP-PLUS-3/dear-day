c93f0c3f9a70ba2545c9676f2f856135f8c5ef2f
Jeonghyun <limithyunii@gmail.com>
2025-02-23 16:52:21 +0900

    ✨ HeartButton 구현

---
+++ components/Button/HeartButton/HeartButton.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import React, { useState } from "react";
+import styles from "./HeartButton.module.scss";
+import Icon from "@/components/Icon/Icon";
+import { useRouter } from "next/navigation";
+
+const HeartButton = () => {
+  const [liked, setLiked] = useState(false);
+  const router = useRouter();
+
+  const toggleLike = () => {
+    if (!window.location.pathname.includes("/member")) {
+      router.push("/login");
+    }
+
+    setLiked((prev) => !prev);
+  };
+
+  return (
+    <button className={styles.heartButton} onClick={toggleLike}>
+      {liked ? <Icon id="filled-heart" /> : <Icon id="heart" />}
+    </button>
+  );
+};
+
+export default HeartButton;
