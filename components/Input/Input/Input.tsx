0ea26c4c0734a8014956be7fdcd4aaddd2ddce2f
Son Ahyun <dkgus731@naver.com>
2025-02-23 22:40:29 +0900

    🔨 (#5) Input 컴포넌트에서 name속성  추가

--- components/Input/Input/Input.tsx
+++ components/Input/Input/Input.tsx
@@ -4,12 +4,14 @@ import { ChangeEvent } from "react";
 import styles from "./input.module.scss";
 
 interface InputProps {
+  name: string;
   value: string;
   onChange: (event: ChangeEvent<HTMLInputElement>) => void;
   placeholder?: string;
 }
 
 const Input = ({
+  name,
   value,
   onChange,
   placeholder = "이름을 입력해주세요 ex) 디어데이 생일카페",
@@ -18,6 +20,7 @@ const Input = ({
     <div className={styles.inputContainer}>
       <input
         type="text"
+        name={name}
         value={value}
         onChange={onChange}
         placeholder={placeholder}
