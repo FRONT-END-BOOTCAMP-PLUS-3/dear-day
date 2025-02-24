53cafbbdb625d9809e4bc7807ebc58e2eb1bd47a
gkdud7351 <gkdud7351@gmail.com>
2025-02-24 16:53:11 +0900

    📝 (#14) RegisterEventStep3 파일 생성

---
+++ components/RegisterEvent/RegisterEventStep3.tsx
@@ -0,0 +1,14 @@
+import styles from "./RegisterEvent.module.scss";
+
+const RegisterEventStep3 = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.mainImage}></div>
+      <div className={styles.detailImage}></div>
+      <div className={styles.mainBenefits}></div>
+      <div className={styles.subBenefits}></div>
+    </div>
+  );
+};
+
+export default RegisterEventStep3;
