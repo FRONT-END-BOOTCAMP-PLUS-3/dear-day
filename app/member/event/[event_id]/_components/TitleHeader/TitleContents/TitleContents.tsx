69060ca9ec43154dd94c5973581a3f6a608be8f5
Son Ahyun <dkgus731@naver.com>
2025-02-25 23:23:50 +0900

    ✨ (#53) TitleHeader 마크업개발

--- app/member/event/[event_id]/_components/TitleHeader/TitleContents/TitleContents.tsx
+++ app/member/event/[event_id]/_components/TitleHeader/TitleContents/TitleContents.tsx
@@ -1,5 +1,30 @@
+import HeartButton from "@/components/Button/HeartButton/HeartButton";
 import styles from "./TitleContents.module.scss";
+import Icon from "@/components/Icon/Icon";
 
 export default function TitleContents() {
-  return <div className={styles.titleBox}>네모박스</div>;
+  return (
+    <div className={styles.titleBox}>
+      <div>
+        <h1>{"생카 제목"}</h1>
+        <HeartButton eventId={0} />
+      </div>
+      <div>
+        <Icon id="x-logo" />
+        <p>{"트위터 아이디"}</p>
+      </div>
+      <div>
+        <Icon id="calendar" />
+        <p>{"날짜"}</p>
+      </div>
+      <div>
+        <Icon id="map" />
+        <p>{"주소"}</p>
+      </div>
+      <div>
+        <Icon id="profile" />
+        <p>{"생카 주인공"}</p>
+      </div>
+    </div>
+  );
 }
