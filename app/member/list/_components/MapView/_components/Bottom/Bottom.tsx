3fb0cf93933c54a1d4fd856702a1e6ccb08fd167
Son Ahyun <dkgus731@naver.com>
2025-02-25 16:21:19 +0900

    ♻️ (#42) 바텀시트에 데이터 넣기

--- app/member/list/_components/MapView/_components/Bottom/Bottom.tsx
+++ app/member/list/_components/MapView/_components/Bottom/Bottom.tsx
@@ -1,15 +1,14 @@
 "use client";
 
 import BottomSheet from "@/components/BottomSheet/BottomSheet";
+import ListView from "../../../ListView/ListView";
 
 export default function Page() {
   return (
     <div>
       {/* 바텀 시트가 열릴 때만 렌더링 */}
       <BottomSheet>
-        <div>
-          <p>여기에 리스트 넣기</p>
-        </div>
+        <ListView />
       </BottomSheet>
     </div>
   );
