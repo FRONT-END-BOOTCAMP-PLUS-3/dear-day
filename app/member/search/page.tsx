1e7a60eeac3526652c0aec179db595acda0b9928
jin62413 <jin62413@gmail.com>
2025-02-25 19:42:28 +0900

    ✨ (#55) search 페이지 마크업
    - anon 경로 검색결과 페이지
    - member 경로 검색결과 페이지

---
+++ app/member/search/page.tsx
@@ -0,0 +1,11 @@
+import SearchResultPage from "@/app/(anon)/search/[star_id]/page";
+
+const MemberSearchResultPage = () => {
+  return (
+    <>
+      <SearchResultPage />
+    </>
+  );
+};
+
+export default MemberSearchResultPage;
