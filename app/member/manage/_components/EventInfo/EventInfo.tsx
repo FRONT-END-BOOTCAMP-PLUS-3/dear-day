035a4b4f8fe25f0845c31a3e5bf0d7f476c75e6a
jin62413 <jin62413@gmail.com>
2025-02-26 15:55:38 +0900

    ✨ (#57) 생카 관리 페이지 마크업
    - manage 페이지 생성

---
+++ app/member/manage/_components/EventInfo/EventInfo.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import Image from "next/image";
+import styles from "./EventInfo.module.scss";
+import Icon from "@/components/Icon/Icon";
+
+interface EventInfoProps {
+  title: string;
+  imgSrc: string;
+  address: string;
+  startDate: Date;
+  endDate: Date;
+  mode: "RESERVATION" | "WAITTING";
+}
+
+const EventInfo: React.FC<EventInfoProps> = ({
+  title,
+  imgSrc,
+  address,
+  startDate,
+  endDate,
+  mode,
+}) => {
+  const formatShortDate = (date: Date): string => {
+    const year = date.getFullYear().toString();
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const day = String(date.getDate()).padStart(2, "0");
+    return `${year}.${month}.${day}`;
+  };
+
+  return (
+    <div className={styles.eventInfoContainer}>
+      <Image
+        src={imgSrc}
+        alt={`${title}포스터`}
+        width={107}
+        height={127}
+        className={styles.eventInfoImg}
+      />
+      <div className={styles.eventInfoContent}>
+        <h3 className={styles.eventInfoTitle}>{title}</h3>
+        <div className={styles.eventInfoLocation}>
+          <Icon id={"map"} />
+          <p>{address}</p>
+        </div>
+        <p>
+          <time dateTime={startDate.toISOString()}>
+            {formatShortDate(startDate)}
+          </time>
+          &nbsp;~&nbsp;
+          <time dateTime={endDate.toISOString()}>
+            {formatShortDate(endDate)}
+          </time>
+        </p>
+        <p>{mode === "RESERVATION" ? "예약 시스템" : "대기 시스템"}</p>
+      </div>
+    </div>
+  );
+};
+
+export default EventInfo;
