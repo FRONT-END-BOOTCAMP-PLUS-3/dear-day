ba95203380dfa8f7db534986188d8d75d9047e90
Son Ahyun <dkgus731@naver.com>
2025-02-28 14:20:55 +0900

    ✨ (#76) 리포지토리 구현

---
+++ infrastructure/repositories/PgEventRepository.ts
@@ -0,0 +1,20 @@
+import { PrismaClient, Event } from "@prisma/client";
+import { EventRepository } from "@/domain/repositories/EventRepository";
+
+const prisma = new PrismaClient();
+
+export class PgEventRepository implements EventRepository {
+  // 이벤트 아이디로 이벤트 찾기
+  async findEventByEventId(eventId: number): Promise<Event | null> {
+    try {
+      return await prisma.event.findUnique({
+        where: { id: eventId },
+      });
+    } catch (error) {
+      console.error("이벤트 조회 중 오류 발생:", error);
+      throw new Error("이벤트를 불러오는 중 오류가 발생했습니다.");
+    } finally {
+      await prisma.$disconnect();
+    }
+  }
+}
