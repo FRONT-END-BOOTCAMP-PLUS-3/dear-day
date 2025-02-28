96c24b2231a94500016e5b584e88073d5aa0bf3b
Son Ahyun <dkgus731@naver.com>
2025-02-28 14:00:48 +0900

    🔨  (#76) 반환 타입 변경

--- domain/repositories/ReservationRepository.ts
+++ domain/repositories/ReservationRepository.ts
@@ -1,5 +1,5 @@
 import { Reservation } from "@prisma/client";
 
 export interface ReservationRepository {
-  findAllReservationsById(id: string): Promise<Reservation[] | null>; // 이벤트 아이디로 예약명단 찾는 메서드
+  findAllReservationsById(id: string): Promise<Reservation[]>; // 이벤트 아이디로 예약명단 찾는 메서드
 }
--- domain/repositories/WaitingRepository.ts
+++ domain/repositories/WaitingRepository.ts
@@ -1,5 +1,5 @@
 import { Waiting } from "@prisma/client";
 
 export interface WaitingRepository {
-  findAllWaitingsById(id: string): Promise<Waiting[] | null>; // 이벤트 아이디로 예약명단 불러오는 메서드
+  findAllWaitingsById(id: string): Promise<Waiting[]>; // 이벤트 아이디로 예약명단 불러오는 메서드
 }
