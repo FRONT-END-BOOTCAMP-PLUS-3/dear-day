9a612b644175143eb00a51ee266a01a67480260e
jin62413 <jin62413@gmail.com>
2025-03-03 15:47:21 +0900

    🔥 (#78) NextButton 코드 되돌리기
    - submit 옵션 필요 없어서 삭제

--- components/Button/NextButton/NextButton.tsx
+++ components/Button/NextButton/NextButton.tsx
@@ -3,24 +3,17 @@
 import styles from "./NextButton.module.scss";
 
 interface NextButtonProps {
-  type: "button" | "submit";
-  onSubmit?: () => void;
+  type: "button";
   onClick?: () => void;
   value: string;
   disabled?: boolean;
 }
 
-const NextButton = ({
-  onSubmit,
-  onClick,
-  value,
-  disabled = false,
-}: NextButtonProps) => {
+const NextButton = ({ onClick, value, disabled = false }: NextButtonProps) => {
   return (
     <button
       className={`${styles.nextButtonContainer} ${disabled ? styles.disabled : ""}`}
       onClick={!disabled ? onClick : undefined}
-      onSubmit={!disabled ? onSubmit : undefined}
     >
       <p>{value}</p>
     </button>
