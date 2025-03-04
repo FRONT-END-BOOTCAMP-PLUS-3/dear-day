68e44ee3a572df7d06e74053946bcd5bff628e2d
Son Ahyun <dkgus731@naver.com>
2025-03-04 10:46:26 +0900

    ✨ (#87) CheckReservationAvailability 관련 API 생성

---
+++ app/api/event/reservation/[event_id]/route.ts
@@ -0,0 +1,36 @@
+import { NextResponse } from "next/server";
+import { PgReservationRepository } from "@/infrastructure/repositories/PgReservationRepository";
+import { CheckReservationAvailability } from "@/application/usecases/event/CheckReservationAvailability";
+
+export async function GET(req: Request) {
+  try {
+    const { searchParams } = new URL(req.url);
+    const eventId = Number(searchParams.get("[event_id]"));
+    if (!eventId) {
+      return NextResponse.json(
+        { error: "eventId가 필요합니다." },
+        { status: 400 }
+      );
+    }
+    const reservationRepository = new PgReservationRepository();
+
+    try {
+      const reservationList = await CheckReservationAvailability(
+        eventId,
+        reservationRepository
+      );
+      return NextResponse.json(reservationList);
+    } catch (error) {
+      if (error.message === "이벤트를 찾을 수 없습니다.") {
+        return NextResponse.json(
+          { error: "예약 목록을 찾을 수 없습니다." },
+          { status: 404 }
+        );
+      }
+      throw error; // 다른 예상치 못한 에러는 그대로 던짐
+    }
+  } catch (error) {
+    console.error("서버 오류 발생:", error);
+    return NextResponse.json({ error: "서버 오류 발생" }, { status: 500 });
+  }
+}
