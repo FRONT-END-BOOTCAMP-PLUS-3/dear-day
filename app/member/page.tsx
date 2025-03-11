d61fbf813072e72559335fb8185c5e98f23a1a25
gkdud7351 <gkdud7351@gmail.com>
2025-03-11 14:32:09 +0900

    ➖ 임시 로그아웃 버튼 삭제

--- app/member/page.tsx
+++ app/member/page.tsx
@@ -6,18 +6,10 @@ import MainButton from "../(anon)/_components/MainButton/MainButton";
 import Banner from "../(anon)/_components/Banner/Banner";
 import styles from "./page.module.scss";
 import SearchInput from "@/components/Input/SearchInput/SearchInput";
-import { useAuthStore } from "@/store/authStore";
-import { useRouter } from "next/navigation";
 
 export default function HomePage() {
-  const logout = useAuthStore((state) => state.logout);
-  const router = useRouter();
   const [searchWord, setSearchWord] = useState("");
 
-  async function handleLogout() {
-    await logout();
-    router.push("/login"); // ✅ 로그아웃 후 로그인 페이지로 이동
-  }
   const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
     setSearchWord(e.target.value);
   };
@@ -26,7 +18,6 @@ export default function HomePage() {
     <div className={styles.homeContainer}>
       <Banner />
       <div className={styles.contentsWrapper}>
-        <button onClick={handleLogout}>로그아웃</button>
         <SearchInput value={searchWord} onChange={handleSearch} />
         <MainButton />
         <UpcomingEvent />
