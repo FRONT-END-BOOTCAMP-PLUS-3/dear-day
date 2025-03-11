dbf904bee7cf4e66f770ad221f3f6aa31d05d64c
gkdud7351 <gkdud7351@gmail.com>
2025-03-11 14:28:55 +0900

    ✨ (#159) 로그아웃

--- app/member/mypage/setting/page.tsx
+++ app/member/mypage/setting/page.tsx
@@ -1,8 +1,22 @@
+"use client";
+
 import BackHeader from "@/components/Header/BackHeader/BackHeader";
 import styles from "./page.module.scss";
 import Icon from "@/components/Icon/Icon";
+import useToggle from "@/hooks/useToggle";
+import Modal from "@/components/modal/Modal";
+import { useAuthStore } from "@/store/authStore";
+import { useRouter } from "next/navigation";
 
 const Page = () => {
+  const [isModalOpen, toggleModal] = useToggle(false);
+  const logout = useAuthStore((state) => state.logout);
+  const router = useRouter();
+
+  const handleConfirm = async () => {
+    await logout();
+    router.push("/");
+  };
   return (
     <div className={styles.homeContainer}>
       <BackHeader title={"내 정보"} />
@@ -10,7 +24,7 @@ const Page = () => {
         <p>알림 설정하기</p>
         <Icon id="arrow-right" />
       </div>
-      <div className={styles.item}>
+      <div className={styles.item} onClick={toggleModal}>
         <p>로그아웃</p>
         <Icon id="arrow-right" />
       </div>
@@ -18,6 +32,19 @@ const Page = () => {
         <p>탈퇴하기</p>
         <Icon id="arrow-right" />
       </div>
+      <Modal
+        contents={[
+          {
+            type: "textOnly",
+            title: "정말 로그아웃 하시겠습니까?",
+          },
+        ]}
+        onConfirm={handleConfirm}
+        onCancel={toggleModal}
+        isOpen={isModalOpen}
+        confirmText="완료"
+        cancelText="취소"
+      />
     </div>
   );
 };
