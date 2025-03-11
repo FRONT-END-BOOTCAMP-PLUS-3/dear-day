eef2afe0b08d278a9a9008995a84bfaccdbceaee
gkdud7351 <gkdud7351@gmail.com>
2025-03-11 15:27:21 +0900

    ✨ 로그인/비로그인 회원 마이페이지 이동 경로 분리

--- components/Header/MainHeader/MainHeader.tsx
+++ components/Header/MainHeader/MainHeader.tsx
@@ -4,8 +4,21 @@ import Icon from "@/components/Icon/Icon";
 import style from "./MainHeader.module.scss";
 import Link from "next/link";
 import Image from "next/image";
+import { useAuthStore } from "@/store/authStore";
+import { useRouter } from "next/navigation";
 
 const MainHeader = () => {
+  const router = useRouter();
+  const user = useAuthStore((state) => state.user);
+
+  const handleProfileClick = (e: React.MouseEvent) => {
+    e.preventDefault();
+    if (user) {
+      router.push("/member/mypage");
+    } else {
+      router.push("/login");
+    }
+  };
   return (
     <header className={style.mainHeader}>
       <Link href="/" className={style.logoLink}>
@@ -21,9 +34,9 @@ const MainHeader = () => {
         <Link href="/notice" className={style.icon}>
           <Icon id="notification" />
         </Link>
-        <Link href="/mypage" className={style.icon}>
+        <button className={style.icon} onClick={handleProfileClick}>
           <Icon id="profile" />
-        </Link>
+        </button>
       </div>
     </header>
   );
