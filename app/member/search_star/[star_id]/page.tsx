ddf46afc1acb54fe0a5691b16c77084d4c146a6c
Son Ahyun <dkgus731@naver.com>
2025-03-11 22:05:53 +0900

    🔨(#163) /member/search_star/[star_id]에서 스토어에 타이틀 저장하기

--- app/member/search_star/[star_id]/page.tsx
+++ app/member/search_star/[star_id]/page.tsx
@@ -5,11 +5,13 @@ import styles from "./page.module.scss";
 import { useEffect, useState } from "react";
 import { searchEventListByStarDto } from "@/application/usecases/event/dto/SearchEventListByStarDto";
 import { useParams } from "next/navigation";
+import { useHeaderStore } from "@/store/HeaderStore"; // Zustand import
 
 const MemberSearchResultPage = () => {
   const [eventList, setEventList] = useState<searchEventListByStarDto[]>([]);
   const params = useParams();
   const starId = params.star_id;
+  const setTitle = useHeaderStore((state) => state.setTitle); // Zustand의 setTitle 가져오기
 
   useEffect(() => {
     if (!starId) {
@@ -34,13 +36,16 @@ const MemberSearchResultPage = () => {
         const { data } = await response.json();
         console.log(data);
         setEventList(data);
+        if (data.length > 0) {
+          setTitle(data[0].starName); // starName 설정
+        }
       } catch (error) {
         console.log("목록 조회 실패: ", error);
         setEventList([]);
       }
     };
     fetchSearchEvent();
-  }, [starId]);
+  }, [starId, setTitle]);
 
   return (
     <div className={styles.homeContainer}>
