defe62964782f632d371b693baa9b5adfc94224e
Son Ahyun <dkgus731@naver.com>
2025-03-12 19:54:30 +0900

    💄 헤더

--- components/Header/MainHeader/MainHeader.tsx
+++ components/Header/MainHeader/MainHeader.tsx
@@ -4,21 +4,8 @@ import Icon from "@/components/Icon/Icon";
 import style from "./MainHeader.module.scss";
 import Link from "next/link";
 import Image from "next/image";
-import { useAuthStore } from "@/store/authStore";
-import { useRouter } from "next/navigation";
 
 const MainHeader = () => {
-  const router = useRouter();
-  const user = useAuthStore((state) => state.user);
-
-  const handleProfileClick = (e: React.MouseEvent) => {
-    e.preventDefault();
-    if (user) {
-      router.push("/member/mypage");
-    } else {
-      router.push("/login");
-    }
-  };
   return (
     <header className={style.mainHeader}>
       <Link href="/" className={style.logoLink}>
@@ -34,9 +21,9 @@ const MainHeader = () => {
         {/* <Link href="/notice" className={style.icon}>
           <Icon id="notification" />
         </Link> */}
-        <button className={style.icon} onClick={handleProfileClick}>
+        <Link href="/member/mypage" className={style.icon}>
           <Icon id="profile" />
-        </button>
+        </Link>
       </div>
     </header>
   );
