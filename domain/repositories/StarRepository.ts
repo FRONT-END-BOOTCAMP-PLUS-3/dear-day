39617116e1beb5e203d1461ebfb1c995d4cdaa6e
gkdud7351 <gkdud7351@gmail.com>
2025-03-13 00:30:06 +0900

    ✨ (#196) 스타 프로필 불러오기

--- domain/repositories/StarRepository.ts
+++ domain/repositories/StarRepository.ts
@@ -1,4 +1,5 @@
 import { UserLikedStarDto } from "@/application/usecases/mypage/dto/UserLikedStarDto";
+import { StarProfileDto } from "@/application/usecases/star/dto/StarProfileDto";
 import { Star } from "@prisma/client";
 
 export interface StarRepository {
@@ -6,5 +7,6 @@ export interface StarRepository {
   findStarByStarId(starId: number): Promise<Star | null>; // 스타 아이디로 스타 정보 받아오기
   findAllStarsByKeyword(keyword: string): Promise<Star[]>;
   findLikedStarsByUserId(userId: string): Promise<UserLikedStarDto[]>; // 유저 아이디로 스타 정보 가져오기
+  findStarProfile(starId: number): Promise<StarProfileDto>;
   findAll(): Promise<Star[]>;
 }
