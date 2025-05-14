1e571155a887fd92971127d1cd60809884a11352
Jeonghyun <limithyunii@gmail.com>
2025-05-14 23:12:14 +0900

    🔧 (#230) PWA 설정 시 SSE 연결은 제외 처리 추가

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,14 @@ export const nextConfig: NextConfig = {
 
 export const withPWA = require("next-pwa")({
   dest: "public",
+  disable: process.env.NODE_ENV === "development", // 개발 환경에선 PWA 비활성화
+  runtimeCaching: [
+    {
+      // SSE 요청은 캐시하지 않고 네트워크로 직접 연결
+      urlPattern: /^\/api\/sse\/subscribe/,
+      handler: "NetworkOnly",
+    },
+  ],
 });
 
 module.exports = withPWA(nextConfig);
